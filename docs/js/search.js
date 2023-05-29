1ef21dd5a56b605a28497ed67f1e7cc701ea63cb
Vallek <vforval86@gmail.com>
2023-05-29 19:44:05 +0300

    стилизация поиска

--- docs/js/search.js
+++ docs/js/search.js
@@ -2,7 +2,7 @@ SimpleJekyllSearch({
 		searchInput: document.getElementById('search-input'),
 		resultsContainer: document.getElementById('results-container'),
 		json: '/webdevtips/search.json',
-		searchResultTemplate: '<li><a href="{url}" title="{desc}">{title}</a></li>',
+		searchResultTemplate: '<li class="search__item"><a class="search__link" href="{url}">{title}</a></li>',
 		noResultsText: 'No results found',
 		limit: 10,
 		fuzzy: false,
